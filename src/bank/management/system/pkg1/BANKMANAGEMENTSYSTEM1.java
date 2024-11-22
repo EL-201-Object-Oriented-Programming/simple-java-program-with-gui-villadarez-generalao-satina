cf21eee3ea2636104d4b86f5ccc3ee5d4f9348e5
DAEZ <DAEZ@DESKTOP-PQ4ESME>
2024-11-22 22:41:19 +0800

    First Commit!

---
+++ src/bank/management/system/pkg1/BANKMANAGEMENTSYSTEM1.java
@@ -0,0 +1,14 @@
+
+package bank.management.system.pkg1;
+
+/**
+ *
+ * @author DAEZ
+ */
+public class BANKMANAGEMENTSYSTEM1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/bank/management/system/pkg1/ResultSet.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bank.management.system.pkg1;
+
+/**
+ *
+ * @author DAEZ
+ */
+class ResultSet {
+
+    double getDouble(String balance) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
